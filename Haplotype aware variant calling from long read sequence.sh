89f7d38533c83268ffafe1cacc5cb9097aec3fc6
Dr. Kathirvel Kandasamy <k.kathirvel93@gmail.com>
2024-06-21 12:10:07 +0530

    Add files via upload

---
+++ Haplotype aware variant calling from long read sequence.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+reference=$HOME/LongRead/Reference/hg19.fa
+minimap2 -t 64 -d $HOME/LongRead/Reference/hg19_Index.mmi $reference
+Indexed_reference=$HOME/LongRead/Reference/hg19_Index.mmi
+##Quality Check
+cd $HOME/LongRead/fastq
+for i in $(ls *.fastq.gz | uniq); do fastqc ${i} > ${i%}_quality;done
+##Find matching adapter by overlapping reads and trim them
+for i in $(ls *.fastq.gz | uniq); do porechop -i ${i} -o ${i}_trimmed.fastq.gz --threads 64;done
+##Mapping with Reference hg19 by minimap2
+for i in $(ls *_trimmed.fastq.gz | uniq); do minimap2 -t 64 -ax map-ont -a $Indexed_reference ${i} > ${i%}_minimap2.bam;done
+for bam in $(ls *_minimap2.bam); do if test -f $bam; then
+  echo "Mapping is done."
+fi;done
+for bam in $(ls *_minimap2.bam); do if ! test -f $bam; then
+  echo "Problem with Mapping, something isn't correct"
+fi;done
+##Adding Read group information for each bam files with sample and platform details
+for i in $(ls *._minimap2.bam | uniq); do java  -jar  $HOME/LongRead/Softwares/picard/picard.jar AddOrReplaceReadGroups I=${i} O=${i%}_picard.bam RGID=4 RGLB=lib1 RGPL=ILLUMINA RGPU=unit1 RGSM=${i};done
+for bam in $(ls *_picard.bam); do if test -f $bam; then
+  echo "Adding Read group is done."
+fi;done
+for bam in $(ls *_picard.bam); do if ! test -f $bam; then
+  echo "Problem with Adding Read group, something isn't correct"
+fi;done
+##Sorting the bam file
+for i in $(ls *_picard.bam | uniq); do samtools sort ${i} > ${i%}_sorted.bam;done
+##Indexing the bam file
+for i in $(ls *_sorted.bam | uniq); do samtools index ${i} > ${i%}.bai;done
+for i in $(ls *_sorted.bam | uniq); do samtools flagstat ${i} > ${i%}_mapping_stats;done
+##Calling Variants by gatk
+for i in $(ls *_sorted.bam | uniq); do $HOME/LongRead/Softwares/gatk/./gatk --java-options "-Xmx96g" HaplotypeCaller -R $reference -I ${i} -O ${i%}_gatk.vcf;done
+for vcf in $(ls *_gatk.vcf); do if test -f $vcf; then
+  echo "variant calling is done."
+fi;done
+for vcf in $(ls *_gatk.vcf); do if ! test -f $vcf; then
+  echo "Problem with variant calling, something isn't correct"
+fi;done
+
+##snpsift for rsID
+for i in $(ls *_gatk.vcf | uniq); do java "-Xmx96g" -jar $HOME/LongRead/Softwares/snpEff/SnpSift.jar annotate -id $HOME/LongRead/Reference/Homo_sapiens_assembly19.dbsnp138.vcf ${i} > ${i%}_snpsift.vcf;done
+for vcf in $(ls *_snpsift.vcf); do if test -f $vcf; then
+  echo "variant annotation for rsID is done."
+fi;done
+for vcf in $(ls *_snpsift.vcf); do if ! test -f $vcf; then
+  echo "Problem with variant annotation for rsID, something isn't correct"
+fi;done
+##snpeff for predicting the impact of varints
+for i in $(ls *_snpsift.vcf | uniq); do java "-Xmx96g" -jar $HOME/LongRead/Softwares/snpEff/snpEff.jar hg19 ${i} > ${i%}_snpeff.vcf;done
+for vcf in $(ls *_snpeff.vcf); do if test -f $vcf; then
+  echo "Prediction of Impact of variants is done."
+fi;done
+for vcf in $(ls *_snpeff.vcf); do if ! test -f $vcf; then
+  echo "Problem with Prediction of Impact of variants, something isn't correct"
+fi;done
+##Haplotype aware vcf creation
+###Phasing
+bam=(*_sorted.bam)
+vcf=(*_snpeff.vcf)
+for ((i=0; i<${#bam[@]}; i++)); do whatshap phase -o ${i%}_haplotyped_phased.vcf --reference=$reference "${vcf[i]}" "${bam[i]}" ;done
+for vcf in $(ls *_haplotyped_phased.vcf); do if test -f $vcf; then
+  echo "Phasing is done."
+fi;done
+for vcf in $(ls *_haplotyped_phased.vcf); do if ! test -f $vcf; then
+  echo "Problem with Phasing, something isn't correct"
+fi;done
+##Creating Haplotagged BAM
+for i in $(ls *_haplotyped_phased.vcf | uniq); do bgzip ${i};done
+for i in $(ls *_haplotyped_phased.vcf.gz | uniq); do tabix ${i};done
+for i in $(ls *_haplotyped_phased.vcf.gz | uniq); do whatshap stats ${i} > ${i%}_stats.txt;done
+for i in $(ls *_haplotyped_phased.vcf.gz | uniq); do whatshap stats --gtf=${i%}.gtf ${i};done
+bam=(*_sorted.bam)
+vcf=(*_phased.vcf.gz)
+for ((i=0; i<${#bam[@]}; i++)); do whatshap haplotag -o ${i%}_haplotagged.bam --reference $reference "${vcf[i]}" "${bam[i]}";done
+for vcf in $(ls *_haplotagged.bam); do if test -f $vcf; then
+  echo "haplotagged BAM creation is done."
+fi;done
+for vcf in $(ls *_haplotagged.bam); do if ! test -f $vcf; then
+  echo "Problem with haplotagged BAM creation, something isn't correct"
+fi;done
+for i in $(ls *_haplotagged.bam | uniq); do samtools index ${i} > ${i%}.bai;done
+mv *_sorted.bam *_sorted.bam.bai $HOME/LongRead/Final_BAM/
+mv *_gatk.vcf gatk_vcf/
+mv *_snpsift.vcf *_snpeff.vcf $HOME/LongRead/snpeff/
+mv *_haplotyped_phased.vcf *_haplotyped_phased.vcf.gz *_haplotyped_phased.vcf.gz.idx *_stats.txt *.gtf *haplotagged.bam *haplotagged.bam.bai $HOME/LongRead/whatshap/
+mv *_minimap2.bam *_picard.bam *_mapping_stats $HOME/LongRead/Others/
+
+
+
+
+
---
+++ Preparation.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+HOME="/mnt/HPStorage/Kathir"
+mkdir $HOME/LongRead
+cd $HOME/LongRead
+mkdir fastq Reference Mapping Final_BAM gatk_vcf snpeff whatshap Softwares Others
+##Downloading Reference and Indexing
+wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/chromFa.tar.gz
+cat chr1.fa chr2.fa chr3.fa chr4.fa chr5.fa chr6.fa chr7.fa chr8.fa chr9.fa chr10.fa chr11.fa chr12.fa chr13.fa chr14.fa chr15.fa chr16.fa chr17.fa chr18.fa chr19.fa chr20.fa chr21.fa chr22.fa chrX.fa chrY.fa chrM.fa > $HOME/LongRead/Reference/hg19.fa
+if test -f $HOME/LongRead/Reference/hg19.fa; then
+  echo "Downloading hg19 reference genome is done."
+fi 
+if ! test -f $HOME/LongRead/Reference/hg19.fa; then
+  echo "Problem in hg19 reference genome downloading, something isn't correct"
+fi
+mv * $HOME/LongRead/Others/
+##Install fastqc
+cd $HOME/LongRead/Softwares/
+sudo apt-get install fastqc
+##Install minimap2
+sudo apt-get install minimap2
+##picard
+git clone https://github.com/broadinstitute/picard.git
+cd picard/
+./gradlew shadowJar
+cp build/libs/picard.jar ../picard/
+##samtools and bcf tools installation
+cd $HOME/LongRead/Softwares/
+wget https://github.com/samtools/samtools/releases/download/1.20/samtools-1.20.tar.bz2
+tar -xvf samtools-1.20.tar.bz2
+cd samtools-1.20
+./configure
+make
+make install
+##Install gatk
+cd $HOME/LongRead/Softwares/
+wget https://github.com/broadinstitute/gatk/archive/refs/tags/4.5.0.0.tar.gz
+tar -xvf 4.5.0.0.tar.gz
+mv gatk-4.5.0.0/ gatk
+##snpeff and snpsift installation
+wget https://snpeff.blob.core.windows.net/versions/snpEff_latest_core.zip
+unzip snpEff_latest_core.zip
+#rsID annotation and prediction of impact of varints 
+##Download dbsnp 
+wget https://data.broadinstitute.org/snowman/hg19/Homo_sapiens_assembly19.dbsnp138.vcf
+wget https://data.broadinstitute.org/snowman/hg19/Homo_sapiens_assembly19.dbsnp138.vcf.idx
+mv *.vcf *.idx $HOME/LongRead/Reference/
+##Install whatshap 
+pip install whatshap==2.3
